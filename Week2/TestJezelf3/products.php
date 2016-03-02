fc18cbe9d4bc07df0c67b6a035615e5ec6da3f0b
maxvranken <vrankenmax@hotmail.com>
2016-03-02 18:45:30 +0100

    Imd talks final

--- Week2/TestJezelf3/products.php
+++ Week2/TestJezelf3/products.php
@@ -7,7 +7,7 @@ include_once("include_products.php");
 <?php foreach($productsArray as $value => $product): ?>
     <h1><?php echo $product['beschrijving'] . " " . $product['prijs'] ?></h1>
     <img src="<?php echo $product['foto'] ?>" alt="">
-    <a href="/PcLink/Oefeningen/Week2/TestJezelf3/detail.php?product=<?php echo $value ?>"> Meer info</a>
+    <a href="/OefeningenPHP/Week2/TestJezelf3/detail.php?product=<?php echo $value ?>"> Meer info</a>
     <?php endforeach; ?>
 <?php foreach($productsArray as $products => $value) {
 
--- Week2/TestJezelf4/imd_talks_2014/index.php
+++ Week2/TestJezelf4/imd_talks_2014/index.php
@@ -1,4 +1,11 @@
 <?php
+$feedbackname = "";
+$feedbackemail = "";
+$feedbackpassword = "";
+
+$_SESSION['loggedin'] = "false";
+
+
 function canLogin( $p_username, $p_password, $p_fullname)
 {
 	if ( !empty($p_username && $p_password && $p_fullname)) {
@@ -18,6 +25,16 @@ if ( !empty( $_POST ) ) {
 		$_SESSION['loggedin'] = "yes";
 
 		header('Location: createpost.php');
+	} else {
+		if(empty($_POST['name'])) {
+			$feedbackname = "vul een  geldige naam in.";
+		}
+		if(empty($_POST['email'])) {
+			$feedbackemail = "vul een geldige email in.";
+		}
+		if(empty($_POST['password'])) {
+			$feedbackpassword = "vul een geldig wachtwoord in.";
+		}
 	}
 }
 ?>
@@ -34,40 +51,43 @@ if ( !empty( $_POST ) ) {
 	<title>IMD Talks</title>
 	<link rel="stylesheet" href="css/normalize.css">
 	<link rel="stylesheet" href="css/twitter.css">
-	
+
 </head>
 <body>
-<?php if(isset($_SESSION['loggedin'])): ?>
+<?php if(($_SESSION['loggedin'] == "false")): ?>
+
+<?php else: ?>
 	<nav>
 
-			<a href="logout.php">Logout</a>
+		<a href="logout.php">Logout</a>
 
 
 	</nav>
-<?php else: ?>
-
 <?php endif; ?>
 
 	<header>
 		<h1>Welcome to IMD-Talks</h1>
 		<h2>Find out what other IMD'ers are building around you.</h2>
 	</header>
-	
-	<div id="rightside">	
 
-	
+	<div id="rightside">
+
+
 	<section id="signup">
-	
+
 		<h2>New to IMD-Talks? <span>Sign Up</span></h2>
 		<form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
-		<input type="text" name="name" placeholder="Full name" />
-		<input type="email" name="email" placeholder="Email" />
-		<input type="password" name="password" placeholder="Password" />
-		<input type="submit" name="btnSignup" value="Sign up for IMD Talks" />
+			<span><?php echo $feedbackname ?></span>
+			<input type="text" name="name" placeholder="Full name" value="<?php if(isset($_POST['name'])) {echo $_POST['name'];}?>"/>
+			<span><?php echo $feedbackemail ?></span>
+			<input type="email" name="email" placeholder="Email" value="<?php if(isset($_POST['name'])) {echo $_POST['email'];}?>"/>
+			<span><?php echo $feedbackpassword ?></span>
+			<input type="password" name="password" placeholder="Password" value="<?php if(isset($_POST['name'])) {echo $_POST['password'];}?>"/>
+			<input type="submit" name="btnSignup" value="Sign up for IMD Talks"/>
 		</form>
-		
+
 	</section>
-	</div>	
-	
+	</div>
+
 </body>
 </html>
